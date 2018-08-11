ffdef715d7425af989568eab2c255c0ea065f657
Bastien Guyl <bastien@guyl.me>
2018-08-11 21:46:06 +0200

    Remove a random font

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import * as KGALittleSwag from './game/assets/fonts/KGALittleSwag.ttf'
 import { NotEnoughAppleSlices } from './game/NotEnoughAppleSlices'
 
 window.onload = () => {
@@ -9,4 +8,3 @@ window.onload = () => {
 document.querySelector('body').style.margin = '0'
 document.querySelector('body').style.textAlign = 'center'
 document.querySelector('body').style.backgroundColor = 'wheat'
-document.createElement('style').innerHTML = `@font-face { font-family: Party; src: url(${KGALittleSwag}); }`
